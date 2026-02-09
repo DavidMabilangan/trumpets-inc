bce2da1435d288f2a27502b0a3623be93e157ee9
David <mabilangan.davidisaiah.css@gmail.com>
2026-02-09 23:27:02 +0800

    add links

--- store/links.js
+++ store/links.js
@@ -2,32 +2,32 @@ import {defineStore} from 'pinia'
 
 export const useLink = defineStore('link', {
     state: () => ({
-links : [
-    {
-        to: '/',
-        label: 'Home',
-        active: false
-    },
-    {
-        to: '/shows',
-        label: 'Shows',
-        active: false
-    },
-    {
-        to: '/about',
-        label: 'About',
-        active: false
-    },
-    {
-        to: 'https://www.trumpetsplayshop.com/',
-        label: 'workshop',
-        active: false
-    },
-    {
-        to: '/support-us',
-        label: 'Support Us',
-        active: false
-    }
-]
+        links : [
+            {
+                to: '/',
+                label: 'Home',
+                active: false
+            },
+            {
+                to: '/shows',
+                label: 'Shows',
+                active: false
+            },
+            {
+                to: '/about',
+                label: 'About',
+                active: false
+            },
+            {
+                to: 'https://www.trumpetsplayshop.com/',
+                label: 'workshop',
+                active: false
+            },
+            {
+                to: '/support-us',
+                label: 'Support Us',
+                active: false
+            }
+        ]
     })
 })
