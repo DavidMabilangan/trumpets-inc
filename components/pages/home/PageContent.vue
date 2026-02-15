48af7d58106016837cc0840b2679384e8d216f13
David <mabilangan.davidisaiah.css@gmail.com>
2026-02-15 22:39:32 +0800

    add shows and workshop

--- components/pages/home/PageContent.vue
+++ components/pages/home/PageContent.vue
@@ -1,9 +1,16 @@
 <script setup>
     import { defineAsyncComponent } from 'vue';
+import WorkShop from './sections/WorkShop.vue';
 
-    const topofthefold = defineAsyncComponent(() => import('./sections/TopOfTheFold.vue'));
+    const topofthefold = defineAsyncComponent(() => import('./sections/TopOfTheFold'));
+    const shows = defineAsyncComponent(()=> import ('./sections/Shows'))
+    const workshop = defineAsyncComponent(() => import('./sections/WorkShop'))
 </script>
 
 <template>
     <topofthefold />
+
+    <shows/>
+
+    <workshop/>
 </template>
