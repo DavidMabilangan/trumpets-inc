e17c29b58ce4f504babe84cfcb843359fdd0a203
David <mabilangan.davidisaiah.css@gmail.com>
2026-02-15 22:41:14 +0800

    add details

--- components/pages/home/sections/TopOfTheFold.vue
+++ components/pages/home/sections/TopOfTheFold.vue
@@ -4,14 +4,22 @@ import ContainerTemplate from '~/components/templates/wrapper/ContainerTemplate.
 </script>
 
 <template>
-  <ContainerTemplate :templates="['w-1440']">
-    <section :class="$style['top-of-the-fold']">
-      <h1>HELLO</h1>
-    </section>
+  <ContainerTemplate :templates="['w-2000']" :class="styl['top-of-the-fold']">
+    <ContainerTemplate :templates="['w-1440']">
+      <h1 :class="styl['top-of-the-fold__header']">Bringing Stories of Faith to Life Throught Theater</h1>
+    </ContainerTemplate>
   </ContainerTemplate>
 </template>
 
-<style lang="stylus" module>
-.top-of-the-fold
-  color: red
+<style lang="stylus" module='styl'>
+  .top-of-the-fold
+    padding: 300px 0
+    background-image: url('/banners/heroBanner.webp')
+    background-repeat: no-repeat
+    background-size: cover
+    background-position: center right
+    &__header
+      font-size: 60px
+      max-width: 700px
+      
 </style>
