8ac444231347a44fe77e9cb3311fb309f3f205fc
David <163022118+DavidMabilangan@users.noreply.github.com>
2026-02-16 16:33:15 +0800

    update shows and works add shows.js

--- components/pages/home/sections/Shows.vue
+++ components/pages/home/sections/Shows.vue
@@ -1,76 +1,88 @@
 <script setup>
-import ContainerTemplate from '~/components/templates/wrapper/ContainerTemplate.vue';
+import ContainerTemplate from '~/components/templates/wrapper/ContainerTemplate.vue'
 
-
-const shows = computed( () => {
-    return [
-        {
-            title:'The Lion, The witch and the wardrobe',
-            image: '/thumbnails/LWWBanner.webp'
-        },
-        {
-            title: 'The Bluebird of Happiness',
-            image:'/thumbnails/BBBanner.webp'
-        },
-        {
-            title: 'The Horse and his Boy',
-            image:'/thumbnails/THBBanner.webp'
-        }
-    ]
+const shows = computed(() => {
+  return [
+    {
+      title: 'The Lion, The witch and the wardrobe',
+      image: '/thumbnails/LWWBanner.webp',
+    },
+    {
+      title: 'The Bluebird of Happiness',
+      image: '/thumbnails/BBBanner.webp',
+    },
+    {
+      title: 'The Horse and his Boy',
+      image: '/thumbnails/THBBanner.webp',
+    },
+  ]
 })
-
 </script>
 
-
 <template>
-    <ContainerTemplate :templates="['w-1440']" :class="styl['shows']">
-        <div :class="styl['shows__header']">
-            <h1 :class="styl['shows__header-heading']">SHOWS</h1>
-            <NuxtLink to="#" class="btn">
-                <p>ALL SHOWS</p>
-            </NuxtLink>
-        </div>
-        <div :class="styl['shows__items']">
-            <ul :class="styl['shows__items-list']">
-                <li v-for="(item,key) in shows" :key="key"  :class="styl['shows__items-list__item']" >
-                    <NuxtImg :src="item.image" :alt="item.title" :class="styl['shows__items-list__item-img']" preload loading="lazy"/>
-                    <p :class="styl['shows__items-list__item-title']"> {{ item.title }}</p>
-                </li>
-            </ul>
-        </div>
-    </ContainerTemplate>
+  <ContainerTemplate :templates="['w-1440']" :class="styl['shows']">
+    <div :class="styl['shows__header']">
+      <h1 :class="styl['shows__header-heading']">SHOWS</h1>
+      <NuxtLink to="#" class="btn">
+        <p>ALL SHOWS</p>
+      </NuxtLink>
+    </div>
+    <div :class="styl['shows__items']">
+      <ul :class="styl['shows__items-list']">
+        <li
+          v-for="(item, key) in shows"
+          :key="key"
+          :class="styl['shows__items-list__item']"
+        >
+          <div :class="styl['shows__items-list__item-img-wrapper']">
+            <NuxtImg
+              :src="item.image"
+              :alt="item.title"
+              :class="styl['shows__items-list__item-img-wrapper__img']"
+              preload
+              loading="lazy"
+            />
+          </div>
+          <p :class="styl['shows__items-list__item-title']">{{ item.title }}</p>
+        </li>
+      </ul>
+    </div>
+  </ContainerTemplate>
 </template>
 
 <style lang="stylus" module="styl">
-    .shows
-        padding: 100px 0
-        &__header
+.shows
+    padding: 100px 0 0
+    &__header
+        display: flex
+        flex-direction: row nowrap
+        justify-content: space-between
+        align-items: center
+        &-heading
+            font-size: 60px
+    &__items
+        &-list
             display: flex
-            flex-direction: row nowrap
-            justify-content: space-between
-            align-items: center
-            &-heading
-                font-size: 60px
-        &__items
-            &-list
+            flex-flow: row nowrap
+            justify-content: flex-start
+            padding:0
+            width: 100%
+            margin: 0 -10px
+            &__item
+                flex: 0 0 calc(33.33% - 20px)
                 display: flex
-                flex-flow: row nowrap
-                justify-content: flex-start
-                padding:0
-                width: 100%
-                margin: 0 -10px
-                &__item
-                    flex: 0 0 calc(33.33% - 20px)
-                    display: flex
-                    flex-direction: column
-                    margin: 0 10px
-                    list-style: none
-                    text-align: center
-                    &-img
-                        width: 100%
-                        height: 600px
-                    &-title
-                        font-size: 24px
-                        font-weight: var(--semibold)
-
+                flex-direction: column
+                margin: 0 10px
+                list-style: none
+                text-align: center
+                &-img-wrapper
+                    height: 500px
+                    width: 100%
+                    &__img
+                        height: 100%
+                        object-fit: cover
+                &-title
+                    font-size: 24px
+                    font-weight: var(--semibold)
+                    text-transform: uppercase
 </style>
---
+++ store/shows.js
@@ -0,0 +1,72 @@
+import { defineStore } from 'pinia'
+
+export const useShows = defineStore('shows', {
+  state: () => ({
+    shows: [
+      {
+        thumbnail: '',
+        title: 'First Name',
+        descriptions:
+          "This new staging of First Name is conceptualized to be a celebration full of life, radicalism and energy. <br/ > Staged with highly dramatic lighting and the simplest of sets, First Name takes the audience on a journey through the Biblethrough the eyes of today's generation.The music, composed by Gary Valenciano and Ogie Alcasid, is arranged by Mon Faustino of Power Play and the result is a score that is dynamic, forceful and reflective of the new millennium. <br/ > The libretto and lyrics are by award-winning director and writer Freddie Santos. Hit songs from this show include the internationally-released 'Could You Be Messiah' and 'Woman With No Name' <br/ > This Trumpets original musical, created by the late Freddie Santos is made for families, and the youth and adults of the current generation.",
+        songs: [
+          'What’s in a Name?',
+          'One God Woman',
+          'To Be Named a Disciple',
+          'Woman With No Name',
+          'Could You Be Messiah',
+          'What’s His Name ',
+          'Were You There?',
+          'First Name',
+        ],
+        characters: [''],
+        images: [''],
+      },
+      {
+        thumbnail: '',
+        title: 'Joseph the Dreamer',
+        descriptions:
+          " Joseph the Dreamer is based on the Biblical story of Jacob’s son who saves Egypt with the power of his dreams.<br/ > It is no secret that Jacob favors Joseph above his twelve sons and so he gifts him with a coat of many colors. This display of affection causes the older brothers to resent Joseph. Their ire grows even more when Joseph reveals prophetic dreams that he would one day become lord over all of them. Blinded by rage and jealousy, the brothers plot Joseph’s downfall. <br/ > Faced with one misfortune after another, even imprisonment, Joseph’s faith in God is tested. But with the power of prayer and wisdom from his Mother, he rises above his tribulations, saves Egypt through the blessing of God and his prophetic dreams, and finally reconciles with his family. <br/ > Originally written by the late Freddie Santos, Trumpets’ Joseph the Dreamer is told through vibrant songs and energetic dance numbers all while proclaiming the message to never lose hope in the Lord, and most of all, to 'Never Look Down!'",
+        songs: [
+          'Praise His Name',
+          'The Hand Song',
+          'Like Father',
+          'Like Son',
+          'Colors',
+          'Down with Joseph',
+          ' He Will Carry You',
+          'Lead Me',
+          'Incredible',
+          ' Mae East',
+          'In Moments Like These',
+          ' He Opens a Window',
+          'A New Beginning',
+          'Down and Back',
+          'Everything I Need',
+        ],
+        characters: [
+          ' Joseph',
+          'Jacob',
+          'Asenath',
+          'Rachel',
+          'Potiphar',
+          'Potiphar’s Wife',
+          ' Reuben',
+          'Simeon',
+          'Levi',
+          'Judah',
+          'Dan',
+          'Naphtali',
+          'Gad',
+          'Asher',
+          'Issachar',
+          'Zabulun',
+          'Benjamin',
+          'Young Joseph/Manasseh',
+          'Auctioneer/Pharaoh',
+          'Ensemble',
+        ],
+        images: [''],
+      },
+    ],
+  }),
+})
