86b56253db45c0bd19467a0a06e9fea2a6205174
David <mabilangan.davidisaiah.css@gmail.com>
2026-05-10 22:21:10 +0800

    version-2: make composable auto import and add logo to the header

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,11 +9,22 @@ export default defineNuxtConfig({
         {
           rel: 'stylesheet',
           href: 'https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;500;700&display=swap'
+        },
+        {
+          rel: 'icon',
+          type: 'image/x-icon',
+          href: '/logo.svg'
         }
       ]
     }
   },
   compatibilityDate: '2025-07-15',
+  components: [
+    {
+      path: '~/components/templates',
+      pathPrefix: false
+    }
+  ],
   css: ['@/assets/stylus/default.styl'],
   devtools: { enabled: false },
   modules: [
